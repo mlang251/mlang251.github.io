8f7fa31ef96129c535bf30d597231dbbe8bc01f9
Mark <mlang41110@yahoo.com>
2016-10-13 21:44:52 -0400

    Commented code

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,27 @@
 var main = function() {
 
+	//Create variable for a jQuery selector
 	var $landingNav = $("section#landing ul"); 
 
 	var navigationLayout = function() {
-		$landingNav.removeClass("windowLarge windowSmall");
+		$landingNav.removeClass("windowLarge windowSmall");		//Remove classes initially so that both don't appear at same time
 		if ($(window).width() >= 975) {
-			$landingNav.addClass("windowLarge");
+			$landingNav.addClass("windowLarge");				//On medium and large viewports, landing navigation appears with inline-block list items
 		} else {
-			$landingNav.addClass("windowSmall");
+			$landingNav.addClass("windowSmall");				//Otherwise it appears with block list items
 		}
-	}
+	};
 
-	navigationLayout();
+	navigationLayout();						//Call function as part of main so that the navigation responds to the intial viewport width
 
 
-	$(window).resize(function() {
+	$(window).resize(function() {			//Call function whenever the window gets resized
 		navigationLayout();
 	})
 
 
+
+	//SMOOTH SCROLL FUNCTION
 	$(function() {
 	  $('a[href*="#"]:not([href="#"])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
