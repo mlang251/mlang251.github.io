373ca5e121d951642ee0afd4700bc977fac8c014
Mark <mlang41110@yahoo.com>
2016-10-26 10:41:00 -0400

    simplified Tablist focus handler conditional

--- scripts/script.js
+++ scripts/script.js
@@ -79,7 +79,7 @@ var main = function() {
 
 		//When tablist is focused, and has no currently focused child list item, set the focus to the first child list item
 		this.$id.focus(function() {
-			if (this.hasChildFocus === false) {
+			if (!this.hasChildFocus) {
 				this.setTabIndex(0, tabindexFocus);							//Set tabindex of first child to 0
 				this.$childList[0].focus();			//Give focus to the first child
 				this.hasChildFocus = true;			//Set to true so this function doesn't run again when tablist is focused
