184bfd0186adcb2c8e222d9d8662a4331d506f66
Mark <mlang41110@yahoo.com>
2016-10-26 22:15:16 -0400

    added bindHandlers function to Tablist.prototype

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,7 @@ var main = function() {
 
 
  	//Constructor to work with interactive tablists with ARIA enabled navigation
+ 	//The Tablists are container widgets that contain children that either are anchor elements, or have their own children that are anchor elements
 	function Tablist(selector) {
 		this.$id = $(selector);									//jQuery element for the tablist being constructed
 		this.$children = this.$id.children();					//jQuery object that contains child elements - the elements themselves are jQuery objects
@@ -27,6 +28,27 @@ var main = function() {
 	}
 
 
+	//Member function of Tablist constructor
+	//Binds event handlers to the children of each constructed Tablist
+	Tablist.prototype.bindHandlers = function() {
+		var self = this;								//To avoid confusion with this, set "self" equal to the Tablist that is currently being constructed
+
+		this.$children.click(function(e) {				//In Tablist that is currently being constructed, create event handler for each event.
+			return self.handleClick($(this), e);		//When a child item of the Tablist that is currently being constructed is the target of an event,
+		});												//call the necessary event handler from that same Tablist.
+														//Pass a jQuery element that points to the clicked child element
+		this.$children.keydown(function(e) {
+			return self.handleKeydown($(this), e);		//Inside the event handlers, the scope of "this" is equal to the specific element that fired the event
+		});												//This is why we set self = this at the beginning, so that we don't lose a reference to the Tablist
+
+		this.$children.focus(function() {
+			return self.handleFocus($(this));
+		});
+
+		this.$children.blur(function() {
+			return self.handleBlur($(this));
+		});
+	}
 
 
 
